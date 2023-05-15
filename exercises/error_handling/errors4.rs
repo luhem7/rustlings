917c2f2c194266f4a95eb574ccaf33e3b5075bac
luhem7 <mehul.gang@gmail.com>
2023-05-15 08:03:34 -0400

    Checkpoint on errors

--- exercises/error_handling/errors4.rs
+++ exercises/error_handling/errors4.rs
@@ -15,6 +15,9 @@ enum CreationError {
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
         // Hmm...? Why is this only returning an Ok value?
+        if value < 1 {
+
+        }
         Ok(PositiveNonzeroInteger(value as u64))
     }
 }
